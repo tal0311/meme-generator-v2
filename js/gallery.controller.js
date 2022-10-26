93c88005ed57cda0c28068f992c3c2f43a24fd7b
tal0311 <tal.amit0311@gmail.com>
2022-10-26 14:21:55 +0300

    navigation

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -1,7 +1,28 @@
 function onInit() {
   console.log('%c Gallery init', 'color:lightgreen')
+  navigateTo()
 }
 
-function renderGallery() {
-  const images = getImagesForDisplay()
+function navigateTo(route = 'gallery') {
+  const pages = [...document.querySelectorAll('.page')]
+  pages.map((page) => {
+    return !page.classList.contains(route)
+      ? (page.hidden = true)
+      : (page.hidden = false)
+  })
+  renderBy(route)
+}
+
+function renderBy(route) {
+  switch (route) {
+    case 'gallery':
+      renderGallery(route)
+      break
+    case 'saved':
+      renderSaved(route)
+      break
+    case 'editor':
+      renderEditor(route)
+      break
+  }
 }
