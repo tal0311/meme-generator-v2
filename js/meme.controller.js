43066d43f8468f7b7b1382af7b7a0d8abefd9c86
tal0311 <tal.amit0311@gmail.com>
2022-10-26 17:15:07 +0300

    strting editor

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,1 +1,22 @@
+var gCanvas
+var gCtx
 
+function initEditor() {
+  gCanvas = document.querySelector('canvas')
+  gCtx = gCanvas.getContext('2d')
+}
+
+function renderEditor() {
+  const meme = getMemeForDisplay()
+  if (!meme) {
+    document.querySelector(
+      '.meme-editor'
+    ).innerHTML = `<h1 class="default-msg">Select Meme from galley to edit</h1>`
+    return
+  }
+  const img = new Image()
+  img.src = 'img/1.jpg'
+  img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
+  }
+}
--- js/services.js/meme.service.js
+++ js/services.js/meme.service.js
@@ -1,6 +1,29 @@
 var gSearchCountMap = { funny: 12, cat: 16, baby: 2 }
 
-var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+var gImgs = [
+  { id: 1, url: 'assets/images/1.jpg', keywords: ['politics', 'funny', 'cat'] },
+  { id: 1, url: 'assets/images/2.jpg', keywords: ['poppy', 'funny', 'cat'] },
+  {
+    id: 1,
+    url: 'assets/images/3.jpg',
+    keywords: ['poppy', 'baby', 'funny', 'cat'],
+  },
+  { id: 1, url: 'assets/images/4.jpg', keywords: ['sleep', 'cat'] },
+  { id: 1, url: 'assets/images/5.jpg', keywords: ['baby'] },
+  { id: 1, url: 'assets/images/6.jpg', keywords: ['funny', 'history'] },
+  { id: 1, url: 'assets/images/7.jpg', keywords: ['kid', 'black'] },
+  { id: 1, url: 'assets/images/8.jpg', keywords: ['crazy', 'funny'] },
+  { id: 1, url: 'assets/images/9.jpg', keywords: ['baby'] },
+  { id: 1, url: 'assets/images/10.jpg', keywords: ['obama', 'politics'] },
+  {
+    id: 1,
+    url: 'assets/images/11.jpg',
+    keywords: ['black', 'basket ball', 'love'],
+  },
+  { id: 1, url: 'assets/images/12.jpg', keywords: ['zadik', 'etgar'] },
+  { id: 1, url: 'assets/images/13.jpg', keywords: ['leo', 'hollywood'] },
+  { id: 1, url: 'assets/images/14.jpg', keywords: ['morpheus', 'hollywood'] },
+]
 
 var gMeme = {
   selectedImgId: 5,
@@ -18,4 +41,8 @@ var gMeme = {
 
 function getImagesForDisplay() {
   return gImgs
+}
+
+function getMemeForDisplay() {
+  return null
 }
